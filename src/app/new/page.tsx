940fb1aa8eafcdd769496a514a0d89446e4b0abd
Ninad Apte <ninadapte12345@gmail.com>
2023-10-09 10:58:18 +0530

    First commit

---
+++ src/app/new/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import prisma from "../../../db";
+import { redirect } from "next/navigation";
+
+async function createTodo(data : FormData){
+    "use server"
+
+    const title = data.get("title")?.valueOf()
+
+    if(typeof title !== "string" || title.length === 0 ){
+       throw new Error("Invalid Title  ")
+    }
+    await prisma.todo.create({data : {title,complete : false}})
+
+    redirect("/") 
+
+}
+
+
+export default function Page(){
+    return (<>
+    <header className="flex justify-between items-center mb-4 ">
+      <h1 className="text-2xl">New</h1>
+      <form action={createTodo}>
+        <input type = "text" name = "title" className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"></input>
+      <div className="flex gap-1 justify-end my-3">
+        <Link href=".." className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none ">Cancel</Link>
+        <button type = "submit" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none ">Submit </button>
+      </div>
+      </form>
+     </header>
+    </>)
+}
